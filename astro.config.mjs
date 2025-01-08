dca79d213ec987526217ab64d955cee345b9b988
ivan2214 <bongiovanniivan12@gmail.com>
2025-01-07 21:25:59 -0300

    Actualizar URL del sitio y el sitemap en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://zafiro.vercel.app",
+	site: "https://zafirolules.vercel.app",
 	integrations: [
 		react(),
 		tailwind({

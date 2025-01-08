1558d270bcbc516eae5e6b133f6af8b2c00bb5d1
ivan2214 <bongiovanniivan12@gmail.com>
2025-01-07 21:22:18 -0300

    Agregar configuración de Biome, fuentes de Google y archivo robots.txt; ajustar importaciones y mejorar SEO en Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-      applyBaseStyles: false,
-  })]
+	site: "https://zafiro.vercel.app",
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+	],
 });
